d0c65f7bb1e27553c3ca58965d6c9820a57118f4
devPisa <cesar.pisa@alunos.bpkedu.com.br>
2025-06-04 20:10:56 -0300

    Feat: Implementation DTO

---
+++ src/application/DTO/task.dto.ts
@@ -0,0 +1,11 @@
+export interface TaskDTO {
+  id: string;
+  title: string;
+  description?: string;
+  status: string;
+  creatorId: string;
+  groupId: string;
+  responsibleId?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
