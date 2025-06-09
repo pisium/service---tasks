7c5e68b0a05d598f0a84eacf5792f77f35e55f2a
Nathan Daniel <ndbreier26@gmail.com>
2025-06-09 20:13:45 -0300

    probe

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ import { taskRoutes } from "./presentation/routes/task.routes";
 const app = express();
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.status(200).send('🟢 Server is running');
+});
+
 app.use('/tasks', taskRoutes);
 
 app.listen(3002, () => console.log('Server online na porta 3002 ✅'));
